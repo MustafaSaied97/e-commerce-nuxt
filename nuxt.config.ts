c5d25fa921af179ffa5637cad4fed3d56c6bbd1f
Peter Magdy <peter.magdy@hmaserv.com>
2023-06-21 15:30:25 +0300

    add og

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,5 +20,16 @@ export default defineNuxtConfig({
         styles: true ,
         autoImport: true 
       }
+    },
+    app : {
+      head: {
+        title:'e-commerce',
+        meta:[
+          {property:'og:title',content:'e-commerce'},
+          {property:'og:type',content:'website'},
+          {property:'og:image',content:'https://e7.pngegg.com/pngimages/324/868/png-clipart-online-shopping-e-commerce-retail-digital-marketing-e-commerce-text-retail-thumbnail.png'},
+          {property:'og:description',content:'e-commerce'},
+        ]
+      }
     }
   })
